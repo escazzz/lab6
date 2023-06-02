344554ab92a3d473c32c1dba789baa2be685caef
escazzz <escakhv@gmail.com>
2023-06-02 17:52:13 +0300

    ez

---
+++ CPackConfig.cmake
@@ -0,0 +1,28 @@
+include(InstallRequiredSystemLibraries)
+
+set(CPACK_PACKAGE_CONTACT liza.ezzheleva@mail.com)
+set(CPACK_PACKAGE_VERSION ${PRINT_VERSION})
+set(CPACK_PACKAGE_NAME "solverapp")
+set(CPACK_PACKAGE_DESCRIPTION_FILE ${CMAKE_CURRENT_SOURCE_DIR}/DESCRIPTION)
+set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE)
+set(CPACK_RESOURCE_FILE_README ${CMAKE_CURRENT_SOURCE_DIR}/README.md)
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "solver application")
+set(CPACK_PACKAGE_VENDOR " eli25046486")
+set(CPACK_PACKAGE_PACK_NAME "solver-${PRINT_VERSION}")
+
+set(CPACK_SOURCE_INSTALLED_DIRECTORIES 
+   "${CMAKE_SOURCE_DIR}/solver_application; solver_application"
+   "${CMAKE_SOURCE_DIR}/solver_lib; solver_lib"
+   "${CMAKE_SOURCE_DIR}/formatter_ex_lib; formatter_ex_lib"
+   "${CMAKE_SOURCE_DIR}/formatter_lib; formatter_lib")
+
+set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
+set(CPACK_GENERATOR "DEB;RPM")
+
+set(CPACK_DEBIAN_PACKAGE_VERSION ${PRINT_VERSION})
+set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "all")
+set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Solves quadratic equations")
+
+set(CPACK_RPM_PACKAGE_SUMMARY "Solves quadratic equations")
+
+include(CPack)
